70fd4ae66f9bc0f7e1c174c01ea28624921fcfde
unknown <muhammadashief17@gmail.com>
2024-10-07 17:22:27 +0700

    tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() { 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>Muhammad Ashief 💖 Ikfina Kamaliya R</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+	  <>
+	    <h1 className="text-3xl font-bold text-warnaku text-center">
+	      font body
+	    </h1>
+      <h1 className="text-5xl font-Noto_Sans_Korean text-gray-01 text-center">
+	      font korea
+	    </h1>
+	  </>
   )
 }
 
