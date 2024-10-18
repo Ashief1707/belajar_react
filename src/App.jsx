45a15a6baa5277a990b7efb5677f1715830cbfe1
unknown <muhammadashief17@gmail.com>
2024-10-18 15:26:19 +0700

    update 18 oktober

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import SignInPage from "./pages/signIn";
+import SignInPage from "./pages/SignIn";
 import SignUpPage from "./pages/SignUp";
 import ErrorRoute from "./pages/errorRoute";
 
